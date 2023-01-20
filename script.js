fa29d0a0bb812949d97af81918c3c0bb60b1687a
Kulya1986 <87373027+Kulya1986@users.noreply.github.com>
2023-01-20 12:19:44 +0200

    Adding Remove

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var inputColor1 = document.querySelector("#color1");
-var inputColor2 = document.querySelector("#color2");
+var inputColor1 = document.querySelectorAll(".colorsPallette input")[0];
+var inputColor2 = document.querySelectorAll(".colorsPallette input")[1];
 var h3CSS = document.querySelector("h3"); 
 var body = document.querySelector("body");
 var addButton = document.querySelector(".addColor");
@@ -23,6 +23,7 @@ function setGradient(){
 function addMoreColor(){
     var colorSection=document.querySelector(".colorsPallette");
     colorSection.appendChild(createColorInput());
+    colorSection.appendChild(createColorRemove());
     if (document.querySelectorAll(".colorsPallette input").length>=4)
     {
         addButton.setAttribute("disabled", "");
@@ -34,11 +35,21 @@ function createColorInput(){
     var n = document.querySelectorAll(".colorsPallette input").length+1;
     inputColor.setAttribute("type","color");
     inputColor.setAttribute("name","color"+n);
-    inputColor.setAttribute("id","color"+n);
+    inputColor.setAttribute("class","colorsPallette__color");
     inputColor.setAttribute("value","#ff0000");
     inputColor.addEventListener("input",setGradient);
     return inputColor;
 }
+
+function createColorRemove(){
+    var imgRemove = document.createElement("img");
+    imgRemove.setAttribute("src","images/remove.png");
+    imgRemove.setAttribute("alt","remove");
+    imgRemove.setAttribute("class","colorsPallette__remove");
+    // inputColor.addEventListener("input",setGradient);
+    return imgRemove;
+}
+
 
 window.addEventListener("load",setGradient);
 inputColor1.addEventListener("input", setGradient);
